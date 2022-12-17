08b5667a50ac9da35db4be7d9d8a7e7585597792
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2022-12-17 20:52:26 +0100

    migrate from postgres to mongo, fix swagger try it out feature

--- src/main/java/pl/edu/pk/cosmo/habsatbackend/emailservice/EmailServiceApplication.java
+++ src/main/java/pl/edu/pk/cosmo/habsatbackend/emailservice/EmailServiceApplication.java
@@ -2,8 +2,11 @@ package pl.edu.pk.cosmo.habsatbackend.emailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/")})
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
